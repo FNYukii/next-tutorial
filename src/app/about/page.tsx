3a91894ff30d47423b96aafe452c6b591a9acc5e
FNYukii <forestgreen14689@gmail.com>
2023-10-24 17:25:08 +0900

    feat: Aboutページを作成

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function Page() {
+	
+  return (
+
+    <main className="p-4">
+
+      <h1 className="text-2xl font-bold">About</h1>
+			<p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nesciunt culpa vel tenetur, quaerat nostrum quam sint nemo repellat aliquam quas odit quisquam fugit modi. Accusantium et beatae officia minus!</p>
+			<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum excepturi molestiae eaque, reprehenderit odit vero dolorem eum provident eveniet laudantium quae.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum suscipit eum deserunt odit fugiat mollitia, eaque debitis eligendi pariatur inventore accusantium, quaerat velit illo accusamus labore temporibus voluptatem quisquam cum! Officiis explicabo ut autem, maxime nihil quae porro sed quasi!</p>
+    </main>
+  )
+}
