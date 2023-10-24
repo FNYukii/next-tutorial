d3bd08c002e53ce4a4e7c41230bf57deb89e2f41
FNYukii <forestgreen14689@gmail.com>
2023-10-24 17:25:20 +0900

    feat: Galleryページを作成

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+	
+  return (
+
+    <main className="p-4">
+
+      <h1 className="text-2xl font-bold">Gallery</h1>
+			<p className="mt-2">あとでここに画像を表示</p>
+    </main>
+  )
+}
